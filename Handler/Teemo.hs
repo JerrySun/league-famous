6fc8546d52216244d6853efe243849a3ef7aaa61
Morgan Gibson <mgibson1987@gmail.com>
2012-04-24 23:45:04 -0700

    get data from app

--- Handler/Teemo.hs
+++ Handler/Teemo.hs
@@ -1,8 +1,25 @@
 module Handler.Teemo where
 
-
 import Import
+import Data.Function (on)
+import Data.List (sortBy)
+
+playerScore ::  Player -> Int
+playerScore p = playerUpvotes p - playerDownvotes p 
+
+percent :: Player -> Int
+percent p = f u d
+            where u = playerUpvotes p
+                  d = playerDownvotes p
+                  f 0 0 = 0
+                  f _ 0 = 100
+                  f u d = (u * 100) `div` (u + d)
+                
 
 getTeemoR :: Handler RepHtml
-getTeemoR  = defaultLayout $(widgetFile "teemo")
+getTeemoR = do
+    acid <- getAcid
+    players <- sortBy (flip compare `on` playerScore) <$> query' acid AllPlayers
+    let playersAndRank = players `zip` ([1..] :: [Int])
+    defaultLayout $(widgetFile "teemo")
 
