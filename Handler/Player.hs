829590c3b0bff420a152474bf1b964cf3a643e6a
Morgan Gibson <mgibson1987@gmail.com>
2012-04-29 00:18:01 -0700

    ready the post page

--- Handler/Player.hs
+++ Handler/Player.hs
@@ -1,5 +1,7 @@
 module Handler.Player
-    (getPlayerR) where
+    ( getPlayerR
+    , getPostR
+    ) where
 
 import Import
 import Text.Lucius (luciusFile)
@@ -8,4 +10,10 @@ import Text.Lucius (luciusFile)
 getPlayerR :: Handler RepHtml
 getPlayerR = defaultLayout $ do
     $(widgetFile "player")
+    toWidget $(luciusFile "templates/teemo.lucius")
+
+getPostR :: Handler RepHtml
+getPostR = defaultLayout $ do
+    $(widgetFile "post")
+    toWidget $(luciusFile "templates/player.lucius")
     toWidget $(luciusFile "templates/teemo.lucius")
