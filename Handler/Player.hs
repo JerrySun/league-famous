528d5817cc12879a5d74a8a08072acd74200925e
Morgan Gibson <mgibson1987@gmail.com>
2012-05-04 20:22:57 -0700

    some top-only changes to getting threads and stats

--- Handler/Player.hs
+++ Handler/Player.hs
@@ -11,11 +11,10 @@ import Data.Maybe (fromMaybe)
 getPlayerR :: Name -> Handler RepHtml
 getPlayerR name  = do
     acid <- getAcid
-    player <- maybe404 $ query' acid (GetPlayer name)
+    player <- maybe404 $ query' acid $ PlayerStats name
     ip <- requestIP
     vote <- query' acid $ GetVote ip name
-    postsx <- fmap (fromMaybe [])  $ query' acid $ RecentSummaries name
-    let posts = map (\x -> (fst (head x), snd (head x), map snd (tail x))) postsx
+    threads <- query' acid $ PlayerThreads name
     
     defaultLayout $ do
         $(widgetFile "comments")
@@ -24,25 +23,12 @@ getPlayerR name  = do
 getPostR :: Int -> Handler RepHtml
 getPostR num = do
     acid <- getAcid
-    ((parN, parent):children) <- maybe404 $ query' acid $ GetThread num
-    let name = postPlayer parent
-    player <- maybe404 $ query' acid $ GetPlayer name
+    thread <- maybe404 $ query' acid $ GetThread num
+    let name = threadPlayer thread
+    player <- maybe404 $ query' acid $ PlayerStats name
     ip <- requestIP
     vote <- query' acid $ GetVote ip name
     render <- getUrlRender
     defaultLayout $ do
         $(widgetFile "comments")
         $(widgetFile "post")
-
---     let children = [ Post "HotShotGG" "4/20/2012 @ 4:20 PM"
---                      "Dude, Jellyfishes are lame. I think they are about as dumb as potatos and can't even speak English."
---                      Nothing
---                    , Post "Elementz" "4/20/2012 @ 4:20 PM"
---                      "Does anyone like these?  What are these?!"
---                      (Just $ render (StaticR okapi_jpg))
---                    , Post "scarra" "4/20/2012 @ 4:20 PM"
---                      "It's a horse you dummy and they can't even swim :p"
---                      Nothing
---                    ]
--- jellies ::  Text
--- jellies = "I feel like jellyfish are the best creatures in the universe. How about you guys? Sometimes they can poison you, but I find them to be hilarious overall.Jellyfish (also known as jellies or sea jellies or a stage of the life cycle of Medusozoa) are free-swimming members of the phylum Cnidaria. Medusa is another word for jellyfish, and refers to any free-swimming jellyfish life stages among animals in the phylum. Jellyfish have multiple morphologies that represent cnidarian classes including the Scyphozoa (over 200 species), Staurozoa (about 50 species), Cubozoa (about 20 species), and Hydrozoa (about 1000¿1500 species that make jellyfish and many more that do not)."
