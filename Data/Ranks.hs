99048e33b5036bfedf5652dfb40ac3b6e77fa7d5
Morgan Gibson <mgibson1987@gmail.com>
2012-05-05 15:19:24 -0700

    make Data.Rank compile

--- Data/Ranks.hs
+++ Data/Ranks.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 module Data.Ranks
-    ( Player (..)
+    ( RankStats (..)
     , PlayerStore 
     , empty
     , newPlayer
@@ -16,7 +16,7 @@ import Data.Typeable (Typeable)
 import Data.SafeCopy (base, deriveSafeCopy)
 import qualified Data.Map as M
 import qualified Data.IntMap as I
-import Data.Maybe (fromMaybe)
+import Data.Maybe (fromMaybe, fromJust)
 import Data.Name
 import qualified Data.Text as T
 
@@ -27,7 +27,7 @@ data StoredPlayer = StoredPlayer { playerName :: Name
 
 $(deriveSafeCopy 0 'base ''StoredPlayer)
 
-playerScore :: PlayerStats -> Int
+playerScore :: StoredPlayer -> Int
 playerScore p = playerUpvotes p - playerDownvotes p 
 
 data PlayerStore = PlayerStore { nameMap :: M.Map Name StoredPlayer
@@ -43,30 +43,30 @@ empty :: PlayerStore
 empty = PlayerStore M.empty I.empty
 
 newPlayer ::  Name -> PlayerStore -> PlayerStore
-newPlayer name ranks = if validName (unName name) then 
-                             case M.lookup name (unwrapMap ranks) of
-                                 Nothing -> ranks'
-                                 Just _ -> ranks
-                       else ranks
-                       where ranks' = PlayerStore names' scores'
+newPlayer name store = if validName (unName name) then 
+                             case M.lookup name (nameMap store) of
+                                 Nothing -> store'
+                                 Just _ -> store
+                       else store
+                       where store' = PlayerStore names' scores'
                              p = StoredPlayer name 0 0
-                             names' = M.insert name p $ unwrapMap ranks
-                             scores' = addScore p $ rankMap ranks
+                             names' = M.insert name p $ nameMap store
+                             scores' = addScore p $ scoreMap store
 
 voteMod :: Name -> Int -> Int -> PlayerStore -> Maybe PlayerStore
 voteMod _    0 0 store = Nothing
-voteMod name u d store = fmap (voteModStored u d store) $ getStats name store
+voteMod name u d store = fmap (voteModStored u d store) . M.lookup name . nameMap $ store
 
-voteModStored :: Int -> Int -> PlayerStore -> PlayerStats -> PlayerStore
+voteModStored :: Int -> Int -> PlayerStore -> StoredPlayer -> PlayerStore
 voteModStored u d store stored = setNameMap names . setScoreMap scores $ store
     where names = M.insert name newPlayer . nameMap $ store
           scores = setOldLevel . setNewLevel . scoreMap $ store
-          setOldLevel = if I.null oldScoreLevel
+          setOldLevel = if M.null oldScoreLevel
                             then I.delete oldScore 
                             else I.insert oldScore oldScoreLevel
           setNewLevel = I.insert newScore newScoreLevel
           oldScoreLevel = M.delete name . fromJust . I.lookup oldScore . scoreMap $ store
-          newScoreLevel = M.insert name newPlayer . fromMaybe I.empty . I.lookup newScore . scoreMap $ store
+          newScoreLevel = M.insert name newPlayer . fromMaybe M.empty . I.lookup newScore . scoreMap $ store
           newPlayer = StoredPlayer name (playerUpvotes stored + u) (playerDownvotes stored + d)
           name = playerName stored
           oldScore = playerScore stored
@@ -74,7 +74,7 @@ voteModStored u d store stored = setNameMap names . setScoreMap scores $ store
           
 
 playerCount ::  PlayerStore -> Int
-playerCount = M.size . unwrapMap
+playerCount = M.size . nameMap
 
 data RankStats = RankStats { rankName :: Name
                            , upvotes :: Int
