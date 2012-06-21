213d124f947dac978eb9a30307d9e1a6d5949e76
Jerry Sun <jerrysun88@gmail.com>
2012-06-21 16:46:44 -0700

    Error page and login page.

--- Handler/Misc.hs
+++ Handler/Misc.hs
@@ -2,4 +2,11 @@ module Handler.Misc where
 
 import Import
 
+getFaqR :: Handler RepHtml
 getFaqR = defaultLayout $(widgetFile "faq")
+
+getErrorR :: Handler RepHtml
+getErrorR = defaultLayout $(widgetFile "error")
+
+getUserLoginR :: Handler RepHtml
+getUserLoginR = defaultLayout $(widgetFile "userlogin")
